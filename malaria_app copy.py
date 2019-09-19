d2b416b9bd1b0b052a595236eada86f4081f93c3
David Wan <wanx4910@mylaurier.ca>
2019-09-19 13:58:57 +0800

    First commit

---
+++ malaria_app copy.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pickle
+from flask import Flask, render_template, request
+import keras
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+
+app = Flask(__name__)
+
+loaded_model = pickle.load(open('malariaCNN.pkl','rb'))
+loaded_model._make_predict_function()
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+def ClassPredictor(file):
+    result = loaded_model.predict(file)
+    return result[0]
+
+def process_image(img):
+    img = np.expand_dims(img, axis=0)
+    print(img.shape)
+    result = ClassPredictor(img)
+    return result
+
+@app.route('/result', methods = ['POST'])
+def result():
+    prediction=''
+    if request.method == 'POST':
+        file = request.files['file']
+        img = np.array(image.load_img(file, target_size=(64,64)))
+        result = process_image(img)
+        print("result from model", result)
+        if float(result)<0.5:
+            prediction = 'Infected cell'
+        else:
+            prediction = 'Non-infected cell'
+        print(prediction)
+        return render_template("result.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
